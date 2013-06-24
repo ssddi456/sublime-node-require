b5a7ba6f7cb0c132d63735abfed3310025fed633
Torrent Lee <torrent88410@gmail.com>
2013-06-24 14:16:49 +0800

    update to support requirejs \ node_webkit

--- require_node.py
+++ require_node.py
@@ -8,18 +8,68 @@ import string
 import re
 
 pkg_path = os.path.abspath(os.path.dirname(__file__))
+re_require = r'(var\s\w+\s*=\s*require\([^\(\)]*\))'
+re_require_nw = r'(var\s\w+\s*=\s*global.require\([^\(\)]*\))'
+re_empty   = r'^\s*$'
 
 class RequireNodeCommand(sublime_plugin.TextCommand):
+    def is_inline_require_region( self ):
+        def getlastline(line):
+            prlin = view.line(line.a-1)
+            prlin = view.substr(prlin)
+            return prlin
+          
+        def getthisline(line):
+            return view.substr(line)
+
+        view   = self.view       
+        region = view.sel()[0]
+        line   = view.line(region)
+        return re.match(self.re_require, getthisline(line)) != None and re.match(self.re_require, getlastline(line)) != None
 
     def write_require(self, resolvers, edit):
-
+        view = self.view
         def getlastline(line):
-          prlin = self.view.line(line.a-1)
-          prlin = self.view.substr(prlin)
+          prlin = view.line(line.a-1)
+          prlin = view.substr(prlin)
           return prlin
         
         def getthisline(line):
-          return self.view.substr(line)
+          return view.substr(line)
+
+        def write_node_require ( require_directive ) :
+          lens = 0
+          spos = 0
+
+          if re.match(self.re_require, getthisline(line)) != None :
+            print('this line is require')
+            spos = line.b
+            lens = view.insert(edit, line.b, '\n'+require_directive)
+          elif re.match(self.re_require, getlastline(line)) != None :
+            print('last line is require')
+            spos = line.a
+            lens = view.insert(edit, line.a, require_directive)
+          else :
+            print('last line not require')
+            spos = line.b
+            if re.match(re_empty, getlastline(line)) :
+              lens = view.insert(edit, line.b, require_directive )
+            else :
+              lens = view.insert(edit, line.b, '\n'+require_directive)
+
+          pos = lens + spos
+          view.sel().clear()
+          view.sel().add(sublime.Region(pos))
+
+          view.show(pos)
+
+        def write_requirejs ( module_name, module_path ) :
+          _edit = view.begin_edit('add package')
+          path_point   = ( view.find(r'require\(\[\n', 0 ) or view.find(r'define\(\[\n', 0 ) ).b
+          view.insert(edit, path_point, '\t'+module_path +',\n');
+          module_point = view.find(r'\],function\(\n', path_point).b
+          view.insert(edit, module_point, '\t'+ module_name +',\n');
+          view.end_edit(_edit)
 
         def write(index):
             if index == -1:
@@ -30,33 +80,22 @@ class RequireNodeCommand(sublime_plugin.TextCommand):
                 upperWords = [string.capitalize(word) for word in module_candidate_name.split("-")[1::]]
                 module_candidate_name = string.join(module_candidate_name.split("-")[0:1] + upperWords, "")
 
-            region = self.view.sel()[0]
-            line = self.view.line(region)
+            require_directive = self.node_tpl % (module_candidate_name, get_path(module_rel_path))
+
             
-            re_require = r'(var\s\w+\s*=\s*require\([^\(\)]*\))'
-            require_directive = "var %s = require(%s);" % (module_candidate_name, get_path(module_rel_path))
+           
 
-            lens = 0
-            spos = 0
+            if self.type == 'nodejs' or self.type == 'amdjs':
+              write_node_require( require_directive )
+            elif self.type == 'requirejs' :
+              if self.is_node_webkit :
+                if self.is_inline_require_region() :
+                  write_node_require( require_directive )
+                  return
+              
+              write_requirejs( module_candidate_name, get_path(module_rel_path) )
 
-            if re.match(re_require, getthisline(line)) != None :
-              print('this line is require')
-              spos = line.b
-              lens = self.view.insert(edit, line.b, '\n'+require_directive)
-            elif re.match(re_require, getlastline(line)) != None :
-              print('last line is require')
-              spos = line.a
-              lens = self.view.insert(edit, line.a, require_directive+'\n')
-            else :
-              print('last line not require')
-              spos = line.b
-              lens = self.view.insert(edit, line.b, '\n'+require_directive)
 
-            pos = lens + spos
-            self.view.sel().clear()
-            self.view.sel().add(sublime.Region(pos))
-
-            self.view.show(pos)
         def get_path(path):
             settings = sublime.load_settings(__name__ + '.sublime-settings')
             quotes_type = settings.get('quotes_type')
@@ -140,37 +179,80 @@ class RequireNodeCommand(sublime_plugin.TextCommand):
 
         suggestions = []
         resolvers   = []
-
+        if self.type == None :
+          return
         if len(_folders) != 0 :
           for folder in _folders:
             #create suggestions for all files in the project
-            for root, subFolders, files in os.walk(folder, followlinks=True):
-              if root.startswith(os.path.join(folder, "node_modules")):
+            for root, subFolders, files in os.walk(folder, followlinks=True, topdown=True):
+              #max 3 rescure
+              if root[ self.path_depth:].count(os.path.sep) == 3:
+                  break
+              if root.startswith(os.path.join(folder, "node_modules")) :
                   continue
               if root.startswith(os.path.join(folder, ".git")):
                   continue
+              if root.startswith(os.path.join(folder, ".svn")):
+                  continue
               for file in files:
+
+                  # get ext
+                  file_name, file_ext = os.path.splitext( file )
+                  # js file only
+                  if file_ext != '.js' and file_ext != '.json' :
+                      continue
+                  # module index specific 
                   if file == "index.js":
                       resolvers.append(self.resolve_from_file(root))
                       suggestions.append([os.path.split(root)[1], root])
                       continue
+                  # add sug
                   resolvers.append(self.resolve_from_file(os.path.join(root, file)))
                   suggestions.append([file, root.replace(folder, "", 1) or file])
 
-        print (os.sys.path)
-        #create suggestions for modules in node_module folder
-        [resolvers_from_nm, suggestions_from_nm] = self.get_suggestion_from_nodemodules()
-        resolvers                               += resolvers_from_nm
-        suggestions                             += suggestions_from_nm
+        if (self.type == 'requirejs' and self.is_inline_require_region() ) or self.type != 'requirejs' :
+            #create suggestions for modules in node_module folder
+            [resolvers_from_nm, suggestions_from_nm]     = self.get_suggestion_from_nodemodules()
+            resolvers                                   += resolvers_from_nm
+            suggestions                                 += suggestions_from_nm
 
-        #create suggestions from native modules
-        [resolvers_from_native, suggestions_from_nm] = self.get_suggestion_native_modules()
-        resolvers                                   += resolvers_from_native
-        suggestions                                 += suggestions_from_nm
+            #create suggestions from native modules
+            [resolvers_from_native, suggestions_from_nm] = self.get_suggestion_native_modules()
+            resolvers                                   += resolvers_from_native
+            suggestions                                 += suggestions_from_nm
 
-        #create suggestions from global modules
-        [resolvers_from_native, suggestions_from_nm] = self.get_suggestion_from_nodemodules_g()
-        resolvers                                   += resolvers_from_native
-        suggestions                                 += suggestions_from_nm
-
+            #create suggestions from global modules
+            [resolvers_from_native, suggestions_from_nm] = self.get_suggestion_from_nodemodules_g()
+            resolvers                                   += resolvers_from_native
+            suggestions                                 += suggestions_from_nm
         self.view.window().show_quick_panel(suggestions, self.write_require(resolvers, edit))
+    def __init__(self, view):
+      self.view = view
+      print 'new require instance'
+      file_name, file_ext = os.path.splitext(view.file_name())
+      if file_ext == '.js' :
+        self.type = 'nodejs'
+      else :
+        self.type = None
+        
+      self.node_tpl   = "var %s = require(%s);"
+      self.re_require = re_require
+      self.is_node_webkit = False
+
+      if view.find(r'require\(\[', 0 ) or view.find(r'define\(\[\n', 0 ) :
+        self.type = 'requirejs'
+
+      elif view.find(re_require,0 ) :
+        self.type = 'nodejs'
+      elif view.find(r'define\s*\(\s*function\s*\(\s*require\s*,\s*exports\s*,\s*module\s*\)\s*\{',0) :
+        # for fcking seajs
+        self.type = 'amdjs' 
+      if view.find_all(re_require_nw,0 ) :
+        self.node_tpl = "var %s = global.require(%s);"
+        self.re_require = re_require_nw
+        self.is_node_webkit = True
+
+      if self.type == None :
+        return
+      else :
+        self.path_depth = len ( os.path.dirname(view.file_name()) ) + len(os.path.sep)
